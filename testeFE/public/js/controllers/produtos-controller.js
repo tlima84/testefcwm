803aa26e9d3c4450e3e2bd404a360be10dd12940
tlima84 <andreolli@gmail.com>
2017-05-08 09:32:23 -0300

    Update produtos-controller.js

--- testeFE/public/js/controllers/produtos-controller.js
+++ testeFE/public/js/controllers/produtos-controller.js
@@ -1,15 +1,9 @@
+(function () {
+    "use strict";
 angular.module('testewmfc').controller('ProdutosController', function(TesteBEService) {
 		let vmProdutosController = this;
 
 		function init(){
-			/*
-			let resource = $resource('http://localhost:3021/produtos', {});
-			resource.get().$promise.then(function(retorno) {
-			  vmProdutosController.produtos = retorno;
-			}).catch(erro => {
-				vmProdutosController.mensagem = erro;
-			}); 
-			*/
 			TesteBEService.listarProdutos().then(retorno =>{
 				vmProdutosController.produtos = retorno;
 			}).catch(erro => {
@@ -18,3 +12,4 @@ angular.module('testewmfc').controller('ProdutosController', function(TesteBESer
 		}
 		init();
 });
+}());
