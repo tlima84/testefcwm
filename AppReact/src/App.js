d7b767abac150dd077498d78fffede176a3fed65
tlima84 <andreolli@gmail.com>
2017-05-08 11:30:19 -0300

    Update App.js

--- AppReact/src/App.js
+++ AppReact/src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import MenuItem from 'material-ui/MenuItem';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Lista from './components/Lista'
 
 import Avatar from 'material-ui/Avatar';
 import imgAvatar from './images/mrrobot.jpg';
 import logoMrro from './images/mr-robot-logo.png';
-import IcDash from 'material-ui/svg-icons/action/dashboard';
-import IcRm from 'material-ui/svg-icons/action/delete-forever';
-import IcRootkit from 'material-ui/svg-icons/action/bug-report';
-import IcRansom from 'material-ui/svg-icons/action/https';
+
 
 import {grey900} from 'material-ui/styles/colors';
 import {grey100} from 'material-ui/styles/colors';
@@ -37,18 +32,9 @@ class App extends Component {
        
         <MuiThemeProvider  muiTheme={muiTheme}> 
           <div>
-            <div id="div_menu">
-              <Drawer open={true}>
-                <AppBar title={<img alt="fSociety"src={logoMrro} className="logo-robot"></img>} 
-                showMenuIconButton={false} />
-                <MenuItem primaryText="fSociety '-'board" leftIcon={<IcDash />} />
-                <MenuItem primaryText="rootkits++" disabled={true} leftIcon={<IcRootkit />} />
-                <MenuItem primaryText="ransomWares" disabled={true} leftIcon={<IcRansom />} />
-                <MenuItem primaryText="rm -rf ECorp" disabled={true} leftIcon={<IcRm />} />
-              </Drawer>
-            </div>
+            
             <div id="div_main" className="main">
-              <AppBar title="Board" iconElementRight={ <Avatar src={imgAvatar} />}/>
+              <AppBar title={<div className="img-tam"><img alt="fSociety"src={logoMrro} className="logo-robot"></img></div>}  iconElementRight={ <Avatar src={imgAvatar} />}/>
               <Lista/>
             </div> 
           </div>
